51b6469660e7fcec6459d7d8d39820f390396d92
limerentfox <christi@limerentfox.com>
2017-01-18 16:50:27 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    var $minion = $('img')
+    $minion.css('border', '10px double red')
+    $minion.addClass('tasty')
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.')
+    })
+  }
+
+  function pressIt() {
+    $(document).on('keydown', function(key) {
+      if (key.which == 71){
+        alert('enter was pressed')
+      }
+    })
+  }
+
+
+
+
 
 $(document).ready(function(){
+  getIt()
+  frameIt()
+  submitIt()
 
-// call functions here
-
-});
+})
